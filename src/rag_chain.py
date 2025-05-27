e03b7a6dd7fb1c66465d849d437e897d29ee2f83
Nousheen-2025 <dr.nousheenshafi@gmail.com>
2025-05-26 23:15:10 -0700

    Create rag_chain.py

---
+++ src/rag_chain.py
@@ -0,0 +1,11 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+def build_chain():
+    db = FAISS.load_local("faiss_index", HuggingFaceEmbeddings())
+    retriever = db.as_retriever()
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    return qa_chain
